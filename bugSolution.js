f65c33a657d969a5698ecb8173a23d8d020e5675
BugHunter-X <askzenai@gmail.com>
2024-12-25 06:06:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+```javascript
+<div class="flex items-center">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shrink-0">
+    Button
+  </button>
+  <p>Some text</p>
+</div>
+```
